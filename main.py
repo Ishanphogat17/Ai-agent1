03296ebadefde7d0d6479db9db87d9c68a7ed83a
Ishanphogat17 <Ishanphogat17@gmail.com>
2026-01-06 11:48:49 +0530

    added prompt tokens and response tokens

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main():
     if api_key == None:
         raise Exception("RuntimeError")
     client = genai.Client(api_key=api_key)
-    tokens_used = prompt_token_count
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash',
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
-    print(f"Prompt tokens: {tokens_used}")
-    print(f"
     print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
