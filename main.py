046022d9840976ff2867e7187b78728bacd94981
Ishanphogat17 <Ishanphogat17@gmail.com>
2026-01-06 12:03:59 +0530

    input taken from the user

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise Exception("RuntimeError")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=args.user_prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
