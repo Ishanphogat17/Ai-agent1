a7ce68ec5b4c990ccd287403728ffb7650eda0e4
Ishanphogat17 <Ishanphogat17@gmail.com>
2026-01-07 11:12:19 +0530

    added context window

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
+
 
 def main():
 #    print("Hello from ai-agent-1!")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
@@ -13,12 +16,12 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-    # Now we can access `args.user_prompt`
+    
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=args.user_prompt
+        contents=messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
